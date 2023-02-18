a01026c7c245e4270c1c4064643cc5e35919f114
Sadril Amin <sadrilshuvo420@gmail.com>
2023-02-19 01:22:27 +0600

    sume done

--- ForLoop.c
+++ ForLoop.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
 int main()
-{
+/*{
   int i;
   for(i=1; i<=15; i++)
     printf("Hello %d\n",i);
+  getch();
+}*/
+{
+  int i, sum;
+  sum=0;
+  for(i=1; i<=5; i++){
+        sum+=i;
+  }
+  printf("The sum is= %d\n", sum);
   getch();
 }
