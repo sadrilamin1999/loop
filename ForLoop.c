ec77ad4bfb39e39f354bfb18bb9ff2b822b1e748
Sadril Amin <sadrilshuvo420@gmail.com>
2023-02-19 11:16:58 +0600

    for loop added

--- ForLoop.c
+++ ForLoop.c
@@ -1,17 +1,9 @@
 #include<stdio.h>
 int main()
-/*{
-  int i;
-  for(i=1; i<=15; i++)
-    printf("Hello %d\n",i);
-  getch();
-}*/
 {
-  int i, sum;
-  sum=0;
-  for(i=1; i<=5; i++){
-        sum+=i;
+  int i;
+  for( i=10; i>=2; i=i-2){
+    printf("%d\n",i);
   }
-  printf("The sum is= %d\n", sum);
-  getch();
+  return 0;
 }
